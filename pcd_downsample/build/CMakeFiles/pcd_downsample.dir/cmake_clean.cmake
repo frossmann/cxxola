99234abe5157fed10f58ab344bfa7cb959938bdb
Francis Rossmann <frossmann@eoas.ubc.ca>
2022-09-22 19:41:53 -0700

    initial commit

---
+++ pcd_downsample/build/CMakeFiles/pcd_downsample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcd_downsample.dir/pcd_downsample.cpp.o"
+  "CMakeFiles/pcd_downsample.dir/pcd_downsample.cpp.o.d"
+  "pcd_downsample"
+  "pcd_downsample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcd_downsample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
